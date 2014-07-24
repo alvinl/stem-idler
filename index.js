461c1e7ab4cb8c175ff060095b45159431eda478
Alvinlz <me@alvinl.com>
2014-07-24 15:46:55 -0400

    Update to reflect changes to Stem core

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 
 module.exports = function (Stem) {
-
-  var bot = Stem.bot;
   
-  Stem.api.addHandler(bot, 'loggedOn', require('./loggedOn'));
+  Stem.api.addHandler('bot', 'loggedOn', require('./loggedOn'));
 
-  Stem.api.addHandler(bot, 'friendMsg', require('./friendMsg'));
+  Stem.api.addHandler('bot', 'friendMsg', require('./friendMsg'));
   
 };
